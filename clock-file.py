9864137bf4bd96b1dbf6b4b3b444a791286153a0
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-03-02 21:29:13 +0800

    .

--- clock-file.py
+++ clock-file.py
@@ -1,1 +1,9 @@
 import tkinter as tk
+from tkinter import messagebox
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+from datetime import datetime, timedelta
+import pytz
+import threading
+import time
+import winsound 
