8fd1883bf1ec5645391d75a49dcd38403ed055d2
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-03-02 21:30:02 +0800

    Added Title

--- clock-file.py
+++ clock-file.py
@@ -7,3 +7,11 @@ import pytz
 import threading
 import time
 import winsound 
+
+class ModernClockApp:
+
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Modern Clock")
+        self.root.geometry("500x650")
+        self.root.resizable(False, False)
