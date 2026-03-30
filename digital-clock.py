689fc0ed11629aa3f41a56fcc4ce8b728052f3a7
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-03-31 01:38:13 +0800

    .

--- digital-clock.py
+++ digital-clock.py
@@ -62,7 +62,7 @@ class ClockApp:
         self.create_clock_tab()
         self.create_world_time_tab()
         self.create_stopwatch_tab()
-        self.create_timer_tab()
+        
         
 
     # ---------------- CLOCK ---------------- #
